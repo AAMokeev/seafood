f1674926123677c3ba2d5f697414418dcfb1b106
Artem Mokeev <artem-mokeev@mail.ru>
2023-07-13 10:13:00 +0300

    Add 'top' section

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.top__slider').slick({
+        prevArrow: '<img src="images/white_arrow-prev.svg" alt="" class="top__slider-arrow-prev">',
+        nextArrow: '<img src="images/white_arrow-next.svg" alt="" class="top__slider-arrow-next">',
+        dots: true,
+        vertical: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    })
+});
